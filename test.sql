4dcedeee3b5fdd3f859ecd8a4856e844cae7b3b5
Jiri Cerny <funkacer@gmail.com>
2021-07-29 19:15:13 +0200

    Multiple interactive sql & ---commands

--- test.sql
+++ test.sql
@@ -1,6 +1,18 @@
+---use:test.db;
 select * from sqlite_master where type = 'table';
-drop table IF EXISTS moje1;
-create table moje1 (id integer primary key autoincrement, value real, code text);
-insert into moje1 values (Null, 122.5, 'Ahoj');
+--drop table IF EXISTS moje1;
+--create table moje1 (id integer primary key autoincrement, value real, code text);
+--insert into moje1 values (Null, 122.5, 'Ahoj');
 select * from sqlite_master;
+
+-- select * from moje1
+select * from moje1 --select * from moje1
+
+;
+---use:a.db;
+select value, code from moje1;
+---use:test.db;
+---insert:moje1;
 select * from moje1;
+
+---save:test.txt;
