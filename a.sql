8a4fc7f42310b01cfc3b646ccdba8eba15df3e5a
Jiří Černý <funkacer@gmail.com>
2021-09-14 21:34:58 +0200

    Corrected test.sql

--- a.sql
+++
@@ -1,2 +0,0 @@
----sqlite3:a.db;
-select * from sqlite_master;
--- test.sql
+++ test.sql
@@ -1,17 +1,24 @@
----use:test.db;
+---sqlite3:test_source.db;
 select * from sqlite_master where type = 'table';
---drop table IF EXISTS moje1;
---create table moje1 (id integer primary key autoincrement, value real, code text);
---insert into moje1 values (Null, 122.5, 'Ahoj');
+drop table IF EXISTS moje1;
+create table moje1 (id integer primary key autoincrement, value real, code text);
+insert into moje1 values (Null, 122.5, 'Ahoj');
+select * from sqlite_master;
+
+---sqlite3:test.db;
+select * from sqlite_master where type = 'table';
+drop table IF EXISTS moje1;
+create table moje1 (id integer primary key autoincrement, value real, code text);
+insert into moje1 values (Null, 122.5, 'Ahoj');
 select * from sqlite_master;
 
 -- select * from moje1
 select * from moje1 --select * from moje1
 
 ;
----use:a.db;
+---sqlite3:test_source.db;
 select value, code from moje1;
----use:test.db;
+---sqlite3:test.db;
 ---insert:moje1;
 select * from moje1;
 
