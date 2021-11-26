528c338b5464894713284122daec5c9969f8870d
Jiří Černý <funkacer@gmail.com>
2021-11-26 18:41:12 +0100

    Command and options - using colors
    Command and options - type intlist

--- ReadSQL.py
+++ ReadSQL.py
@@ -15,6 +15,112 @@ import time
 
 from okno import zobraz
 
+'''
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+print('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+
+'''
+
+RED, YELLOW, GREEN, BLUE, COM, INVGREEN, END = '\033[91m', '\033[33m', '\033[4m', '\033[34m', '\033[4m', '\033[35m\033[42m', '\033[0m'
+printRed = lambda sTxt: print(RED + sTxt + END)
+printYellow = lambda sTxt: print(YELLOW + sTxt + END)
+printBlue = lambda sTxt: print(BLUE + sTxt + END)
+printCom = lambda sTxt: print(COM + sTxt + END)
+printInvGreen = lambda sTxt: print(INVGREEN + sTxt + END)
+Assert = lambda bCond=False, sTxt='': printRed(sTxt) if not bCond else None
+
+conn = None
+sqls = {}
+data = None
+columns = None
+folder_exists = None
+folder_name = None
+db_version = "None: "
+show_cases = 5
+print_max_default = 100
+
+command_options = {}
+command_options["quit"] = {}
+command_options["quit"]["name"] = []
+command_options["quit"]["required"] = []
+command_options["quit"]["type"] = []
+command_options["quit"]["default"] = []
+command_options["quit"]["help1"] = "Help for command 'folder'"
+command_options["quit"]["help2"] = []
+command_options["quit"]["alternative"] = ["q"]
+
+command_options["folder"] = {}
+command_options["folder"]["name"] = ["foldername"]
+command_options["folder"]["required"] = [True]
+command_options["folder"]["type"] = ["str"]
+command_options["folder"]["default"] = [None]
+command_options["folder"]["help1"] = "Help for command 'folder'"
+command_options["folder"]["help2"] = ["Blabla1"]
+command_options["folder"]["alternative"] = ["f"]
+
+command_options["sqlite3"] = {}
+command_options["sqlite3"]["name"] = ["filename"]
+command_options["sqlite3"]["required"] = [True]
+command_options["sqlite3"]["type"] = ["str"]
+command_options["sqlite3"]["default"] = [None]
+command_options["sqlite3"]["help1"] = "Help for command 'folder'"
+command_options["sqlite3"]["help2"] = ["Blabla1"]
+command_options["sqlite3"]["alternative"] = ["f"]
+
+command_options["read"] = {}
+command_options["read"]["name"] = ["filename"]
+command_options["read"]["required"] = [True]
+command_options["read"]["type"] = ["str"]
+command_options["read"]["default"] = [None]
+command_options["read"]["help1"] = "Help for command 'folder'"
+command_options["read"]["help2"] = ["Blabla1"]
+command_options["read"]["alternative"] = ["f"]
+
+command_options["import"] = {}
+command_options["import"]["name"] = ["filename"]
+command_options["import"]["required"] = [True]
+command_options["import"]["type"] = ["str"]
+command_options["import"]["default"] = [None]
+command_options["import"]["help1"] = "Help for command 'folder'"
+command_options["import"]["help2"] = ["Blabla1"]
+command_options["import"]["alternative"] = ["f"]
+
+command_options["mysql"] = {}
+command_options["mysql"]["name"] = ["schema"]
+command_options["mysql"]["required"] = [True]
+command_options["mysql"]["type"] = ["str"]
+command_options["mysql"]["default"] = [None]
+command_options["mysql"]["help1"] = "Help for command 'folder'"
+command_options["mysql"]["help2"] = ["Blabla1"]
+command_options["mysql"]["alternative"] = ["f"]
+
+command_options["insert"] = {}
+command_options["insert"]["name"] = ["tablename"]
+command_options["insert"]["required"] = [True]
+command_options["insert"]["type"] = ["str"]
+command_options["insert"]["default"] = [None]
+command_options["insert"]["help1"] = "Help for command 'folder'"
+command_options["insert"]["help2"] = ["Blabla1"]
+command_options["insert"]["alternative"] = ["f"]
+
+command_options["print"] = {}
+command_options["print"]["name"] = ["what", "from", "to", "step", "list", "columns"]
+command_options["print"]["required"] = [False, False, False, False, False, False]
+command_options["print"]["type"] = [["data","columns"], "int", "int", "int", "intlist", "strlist"]
+command_options["print"]["default"] = ["data", 0, print_max_default, 1, None, None]
+command_options["print"]["help1"] = "Help for command 'folder'"
+command_options["print"]["help2"] = ["Bla1","Bla2","Bla3","Bla4","Bla5","Bla6"]
+command_options["print"]["alternative"] = ["f"]
+
 def parseArgv(argument_list):
 
     parser = argparse.ArgumentParser()
@@ -43,6 +149,7 @@ def parseSql(sql_filename, sql):
 def parseCommand(command_line):
     command = ""
     options = {}
+    error = 0
     #command_line = command_line.replace(" ", "")
     command_line = command_line[1:].lower().strip() #no slash
     print (command_line)
@@ -59,13 +166,13 @@ def parseCommand(command_line):
                         if cll[0].strip() == o:
                             options[o] = cll[1].strip()
                     if cll[0].strip() not in options:
-                        print(f'''Unknown option '{cll[0]}'. I will not use your '{cll[1]}' value in any way.''')
+                        printRed(f'''Unknown option '{cll[0]}'. I will not use your '{cll[1]}' value in any way.''')
                 elif cl != "":
                     if i < len (command_options[c]["name"]):
                         print(f'''I will use '{cl}' for option '{command_options[c]["name"][i]}'.''')
                         options[command_options[c]["name"][i]] = cl
                     else:
-                        print(f'''Too many options given. I will not use your '{cl}' value in any way.''')
+                        printRed(f'''Too many options given. I will not use your '{cl}' value in any way.''')
             break
 
     for i, z in enumerate(zip(command_options[command]["name"], command_options[command]["required"], command_options[command]["default"], command_options[command]["type"])):
@@ -75,19 +182,21 @@ def parseCommand(command_line):
         if r:
             #assert command_options[command]["name"][i] in options
             if n not in options:
-                print(f'''Missing required argument '{n}'. Command won't be executed.''')
+                printRed(f'''Missing required argument '{n}'. Command won't be executed.''')
                 execute = False
-                command = ""
-                options = []
                 break
         if n not in options and d is not None:
             print(f'''Default argument '{n}' set to '{d}'.''')
             options[n] = d
         if n in options:
             if isinstance(t, list):
-                assert options[n] in t, \
-f'''Value {options[n]} is not valid for option '{n}'. \
-Use one of these options: {t}.'''
+                bCond = options[n] in t
+                #print(options[n], t, bCond)
+                sTxt = f'''Value '{options[n]}' is not valid for option '{n}'. Use one of these options: {t}.'''
+                Assert(bCond, sTxt)
+                if not bCond:
+                    execute = False
+                    break
             elif t == "int":
                 try:
                     options[n] = int(options[n])
@@ -116,7 +225,17 @@ Use one of these options: {t}.'''
                         if l_old not in lst_new: lst_new.append(l_old)
                     options[n] = lst_new
 
-    print (f'''Comand '{command}' with options {', '.join([str(str(op) + "=" + str(options[op])) for op in options])}.''')
+    if not execute:
+        command = ""
+        options = []
+    else:
+        printoptions = {}
+        for op in options:
+            if isinstance(options[op], str):
+                printoptions[op] = "'" + options[op] + "'"
+            else:
+                printoptions[op] = options[op]
+        printYellow(f'''Comand '{command}' with options {', '.join([str(str(op) + "=" + str(printoptions[op])) for op in printoptions])}.''')
 
     return command, options
 
@@ -165,13 +284,13 @@ def show_data():
     row_format = "{:>15}" * (len(columns) + 1)
     nrows = len(data)
     if nrows <= show_cases*2:
-        print("There are {} rows. Showing all cases.".format(str(nrows)))
+        printInvGreen("There are {} rows. Showing all cases.".format(str(nrows)))
         print(row_format.format("(Row)", *columns))
         for i, row in enumerate(data):
             #print(row_format.format(str(i), *row)) # not posiible to pass None (Null in db)
             print(row_format.format(str(i+1), *[str(r) for r in row]))    # Null to None
     else:
-        print("There are {} rows. Showing first / last {} cases.".format(str(nrows), str(show_cases)))
+        printInvGreen("There are {} rows. Showing first / last {} cases.".format(str(nrows), str(show_cases)))
         print(row_format.format("(Row)", *columns))
         for i, row in enumerate(data[:show_cases]):
             print(row_format.format(str(i+1), *[str(r) for r in row]))    # Null to None
@@ -423,7 +542,7 @@ def do_sql(sql):
         else:
             print("! Command was not recognized or missing arguments !")
     else:
-        print(db_version + sql + "\n")
+        printBlue(db_version + sql + "\n")
         data_new = None
         try:
             c = conn.cursor()
@@ -458,91 +577,7 @@ def do_sql(sql):
 
 def main(argv):
 
-    global conn, sqls, data, columns, folder_exists, folder_name, db_version, show_cases, command_options, \
-    print_max_default
-
-    conn = None
-    sqls = {}
-    data = None
-    columns = None
-    folder_exists = None
-    folder_name = None
-    db_version = "None: "
-    show_cases = 5
-    print_max_default = 100
-
-    command_options = {}
-    command_options["quit"] = {}
-    command_options["quit"]["name"] = []
-    command_options["quit"]["required"] = []
-    command_options["quit"]["type"] = []
-    command_options["quit"]["default"] = []
-    command_options["quit"]["help1"] = "Help for command 'folder'"
-    command_options["quit"]["help2"] = []
-    command_options["quit"]["alternative"] = ["q"]
-
-    command_options["folder"] = {}
-    command_options["folder"]["name"] = ["foldername"]
-    command_options["folder"]["required"] = [True]
-    command_options["folder"]["type"] = ["str"]
-    command_options["folder"]["default"] = [None]
-    command_options["folder"]["help1"] = "Help for command 'folder'"
-    command_options["folder"]["help2"] = ["Blabla1"]
-    command_options["folder"]["alternative"] = ["f"]
-
-    command_options["sqlite3"] = {}
-    command_options["sqlite3"]["name"] = ["filename"]
-    command_options["sqlite3"]["required"] = [True]
-    command_options["sqlite3"]["type"] = ["str"]
-    command_options["sqlite3"]["default"] = [None]
-    command_options["sqlite3"]["help1"] = "Help for command 'folder'"
-    command_options["sqlite3"]["help2"] = ["Blabla1"]
-    command_options["sqlite3"]["alternative"] = ["f"]
-
-    command_options["read"] = {}
-    command_options["read"]["name"] = ["filename"]
-    command_options["read"]["required"] = [True]
-    command_options["read"]["type"] = ["str"]
-    command_options["read"]["default"] = [None]
-    command_options["read"]["help1"] = "Help for command 'folder'"
-    command_options["read"]["help2"] = ["Blabla1"]
-    command_options["read"]["alternative"] = ["f"]
-
-    command_options["import"] = {}
-    command_options["import"]["name"] = ["filename"]
-    command_options["import"]["required"] = [True]
-    command_options["import"]["type"] = ["str"]
-    command_options["import"]["default"] = [None]
-    command_options["import"]["help1"] = "Help for command 'folder'"
-    command_options["import"]["help2"] = ["Blabla1"]
-    command_options["import"]["alternative"] = ["f"]
-
-    command_options["mysql"] = {}
-    command_options["mysql"]["name"] = ["schema"]
-    command_options["mysql"]["required"] = [True]
-    command_options["mysql"]["type"] = ["str"]
-    command_options["mysql"]["default"] = [None]
-    command_options["mysql"]["help1"] = "Help for command 'folder'"
-    command_options["mysql"]["help2"] = ["Blabla1"]
-    command_options["mysql"]["alternative"] = ["f"]
-
-    command_options["insert"] = {}
-    command_options["insert"]["name"] = ["tablename"]
-    command_options["insert"]["required"] = [True]
-    command_options["insert"]["type"] = ["str"]
-    command_options["insert"]["default"] = [None]
-    command_options["insert"]["help1"] = "Help for command 'folder'"
-    command_options["insert"]["help2"] = ["Blabla1"]
-    command_options["insert"]["alternative"] = ["f"]
-
-    command_options["print"] = {}
-    command_options["print"]["name"] = ["what", "from", "to", "step", "list", "columns"]
-    command_options["print"]["required"] = [False, False, False, False, False, False]
-    command_options["print"]["type"] = [["data","columns"], "int", "int", "int", "intlist", "strlist"]
-    command_options["print"]["default"] = ["data", 0, print_max_default, 1, None, None]
-    command_options["print"]["help1"] = "Help for command 'folder'"
-    command_options["print"]["help2"] = ["Bla1","Bla2","Bla3","Bla4","Bla5","Bla6"]
-    command_options["print"]["alternative"] = ["f"]
+    global folder_name
 
     default_options = 7
     for key1 in command_options.keys():
@@ -568,7 +603,7 @@ but {len(command_options[key1][key2])} '{key2}'.'''
         for sql_filename in sqls.keys():
             #OK_returned = 1
             for i, sql in enumerate(sqls[sql_filename]):
-                print("\n\\\\" + " SQL file '{}' command no {} ".format(sql_filename, str(i+1)) + "\\\\")
+                printCom("\n\\\\" + " SQL file '{}' command no {} ".format(sql_filename, str(i+1)) + "\\\\")
                 #print(sql)
                 #print()
                 OK_returned = do_sql(sql)
@@ -606,7 +641,7 @@ but {len(command_options[key1][key2])} '{key2}'.'''
             parseSql(sql_file, sql)
             #OK_returned = 1
             for i, sql in enumerate(sqls[sql_file]):
-                print(f'''\n\\\\ SQL file '{sql_file}' command no {str(i+1)} \\\\''')
+                printCom(f'''\n\\\\ SQL file '{sql_file}' command no {str(i+1)} \\\\''')
                 #print(sql)
                 #print()
                 OK_returned = do_sql(sql)
--- test/test1_1_insert_sql.sql
+++ test/test1_1_insert_sql.sql
@@ -1,5 +1,5 @@
----folder:test;
----sqlite3:test.dbx;
+\folder test;
+\sqlite3 test.dbx;
 
 select * from sqlite_master where type = "table";
 
@@ -7,6 +7,8 @@ drop table if exists test1;
 
 create table test1 (id INTEGER PRIMARY KEY AUTOINCREMENT, V integer, B integer, D text, U text, SP REAL, SE REAL, ZE text, ZT text, K text);
 
----read:test1.csvx;
+\read test1.csvx;
 
----insert:test1;
+\insert test1;
+
+\print columns;
