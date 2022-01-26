a966423c9674c74f509def71d605b193f4ca05ff
Jiri Cerny <funkacer@gmail.com>
2022-01-26 18:38:09 +0100

    Udacity_5_air_routes

--- 2 Udacity BA/Udacity_5_air_routes.sql
+++ 2 Udacity BA/Udacity_5_air_routes.sql
@@ -1,13 +1,20 @@
 \f "2 Udacity BA";
-\s "Udacity_4_worldcities.db";
+\s "Udacity_5_air_routes.db";
 
-drop table if exists t1;
-create table t1 (id integer primary key autoincrement, City text, Country text);
+drop table if exists t1_routes;
+create table t1_routes (id integer primary key autoincrement, Airline text, Source text, Destination text);
 
-\r "Udacity_4_worldcities1.csv", ",";
-\pd l = [1565, 7233];
-\dp;
-\i t1;
+drop table if exists t1_airlines;
+create table t1_airlines (id integer primary key autoincrement, Code text, "Airline Name" text);
 
-select * from t1;
-\dp;
+drop table if exists t1_airports;
+create table t1_airports (id integer primary key autoincrement, Code text, "Airport Name" text);
+
+\r "Udacity_5_air-routes-sfo1_routes.txt";
+\i t1_routes;
+
+\r "Udacity_5_air-routes-sfo2_airlines.txt";
+\i t1_airlines;
+
+\r "Udacity_5_air-routes-sfo3_airports.txt";
+\i t1_airports;
---
+++ 2 Udacity BA/Udacity_5_air_routes_analyse.sql
@@ -0,0 +1,46 @@
+\f "2 Udacity BA";
+\s "Udacity_5_air_routes.db";
+
+select
+    t1.id, Airline as "AC", "Airline Name", Source as "SC", t3."Airport Name", Destination as "DC", t4."Airport Name"
+    from t1_routes as t1
+left join t1_airlines as t2
+    on t1.airline = t2.code
+left join t1_airports as t3
+    on t1.source = t3.code
+left join t1_airports as t4
+    on t1.destination = t4.code;
+
+select
+    t1.id, Airline as "AC", "Airline Name", Source as "SC", t3."Airport Name", Destination as "DC", t4."Airport Name"
+    from t1_routes as t1
+left join (select * from t1_airlines group by code) as t2
+    on t1.airline = t2.code
+left join (select * from t1_airports group by code) as t3
+    on t1.source = t3.code
+left join (select * from t1_airports group by code) as t4
+    on t1.destination = t4.code;
+
+select code, min("Airline Name"), max("Airline Name"), count(*) as count from t1_airlines group by code having count > 1 order by count desc;
+
+select t1.Code, t1."Airline Name", t2.count from t1_airlines as t1
+inner join (select Code, count(*) as count from t1_airlines group by code having count > 2) as t2
+on t1.code = t2.code
+order by count desc;
+
+select code, min("Airport Name"), max("Airport Name"), count(*) as count from t1_airports group by code having count > 1 order by count desc;
+
+select t2.id, t1.AC, t1.Count, t2."Airline Name", t2."SAirport Name", t2."SC", t2."DAirport Name", t2."DC" from
+(select Airline as "AC", count(*) as Count from t1_routes group by Airline) as t1
+inner join
+(select
+    t1.id, Airline as "AC", "Airline Name", Source as "SC", t3."Airport Name" as "SAirport Name", Destination as "DC", t4."Airport Name" as "DAirport Name"
+    from t1_routes as t1
+left join (select * from t1_airlines group by code) as t2
+    on t1.airline = t2.code
+left join (select * from t1_airports group by code) as t3
+    on t1.source = t3.code
+left join (select * from t1_airports group by code) as t4
+    on t1.destination = t4.code) as t2
+on t1.AC = t2.AC
+order by t1.Count DESC;
