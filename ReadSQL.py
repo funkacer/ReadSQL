5d16f573654e7ce5d6e55bde2171a12144ed00a4
Jiří Černý <funkacer@gmail.com>
2022-01-29 16:35:07 +0100

    Print titles, notes continued

--- ReadSQL.py
+++ ReadSQL.py
@@ -35,11 +35,72 @@ UNDERLINE = '\033[4m'
 print('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
 
 '''
+print (u"\u001b[30m A \u001b[31m B \u001b[32m C \u001b[33m D \u001b[0m")
+print (u"\u001b[34m E \u001b[35m F \u001b[36m G \u001b[37m H \u001b[0m")
+print (u"\u001b[30;1m A \u001b[31;1m B \u001b[32;1m C \u001b[33;1m D \u001b[0m")
+print (u"\u001b[34;1m E \u001b[35;1m F \u001b[36;1m G \u001b[37;1m H \u001b[0m")
+
+import sys
+for i in range(0, 16):
+    for j in range(0, 16):
+        code = str(i * 16 + j)
+        sys.stdout.write(u"\u001b[38;5;" + code + "m " + code.ljust(4))
+    print (u"\u001b[0m")
+
+print (u"\u001b[40m A \u001b[41m B \u001b[42m C \u001b[43m D \u001b[0m")
+print (u"\u001b[44m A \u001b[45m B \u001b[46m C \u001b[47m D \u001b[0m")
+print (u"\u001b[40;1m A \u001b[41;1m B \u001b[42;1m C \u001b[43;1m D \u001b[0m")
+print (u"\u001b[44;1m A \u001b[45;1m B \u001b[46;1m C \u001b[47;1m D \u001b[0m")
+
+import sys
+for i in range(0, 16):
+    for j in range(0, 16):
+        code = str(i * 16 + j)
+        sys.stdout.write(u"\u001b[48;5;" + code + "m " + code.ljust(4))
+    print (u"\u001b[0m")
+
+print (u"\u001b[1m BOLD \u001b[0m\u001b[4m Underline \u001b[0m\u001b[7m Reversed \u001b[0m")
+print (u"\u001b[1m\u001b[4m\u001b[7m BOLD Underline Reversed \u001b[0m")
+
+aa = ["", "\033[1m", "\033[4m", "\033[1m\033[4m"]
+for a in range(3):
+    for i in range(0, 16):
+        for j in range(0, 16):
+            code1 = str(i * 16 + j)
+            code2 = str(j * 16 + i)
+            print(aa[a] + '\033[38;5;' + code1 + 'm' + '\033[48;5;' + code2 + 'm' + " Ahoj " + '\033[0m')
+            #print('\033[48;5;' + code + 'm' + "Ahoj" + '\033[0m')
+
+i = "1"
+while i != "":
+    a = 0
+    i = input()
+    code1 = int(i[:4])
+    if code1 > 3000:
+        code1 -= 3000
+        a = 3
+    if code1 > 2000:
+        code1 -= 2000
+        a = 2
+    if code1 > 1000:
+        code1 -= 1000
+        a = 1
+    if len(i) > 4:
+        code2 = int(i[4:])
+        print(aa[a] + '\033[38;5;' + str(code1) + 'm' + '\033[48;5;' + str(code2) + 'm' + " Ahoj " + '\033[0m')
+    else:
+        print(aa[a] + '\033[38;5;' + str(code1) + 'm' + " Ahoj " + '\033[0m')
+
+
+
 
 colors = {}
 cls = ['\033[1m', '\033[95m', '\033[91m', '\033[33m', '\033[92m', '\033[96m', '\033[4m', '\033[97m\033[42m', '\033[97m\033[101m']
+
 for i in range(9):
     colors[i] = (cls[i], '\033[0m')
+
+
 
 printColor = lambda sTxt, color: print(colors[color][0] + sTxt + colors[color][1])
 
